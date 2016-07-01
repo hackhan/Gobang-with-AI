b23a60c0bdf06d28c626923f0133bc21085c5787
hackhan <604726221@qq.com>
2016-07-01 16:51:24 +0800

    Fixed Minimax algorithm error.

--- ai.cc
+++ ai.cc
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) 2016 Chent, all rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "gobang.h"
 #include "chess_library.h"
 #include <cstdlib>
 #include <climits>
 #include <cstring>
-
 #include <algorithm>
 
-#define LEN_TYPE 5
-int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color);
+int evaluation(const int _y, const int _x, 
+               int (*chessboard)[CK_SIZE], int color);
 /*
  * 博弈树的节点
  */
@@ -24,92 +38,115 @@ typedef struct gametree_node {
 /*
  * 极大极小算法
  */
-void minimax_search(gametree_node *&root, int layer_number,
-                    int &y, int &x) {
-    if (layer_number == LAYER_NUM + 1)
-        return;
+void minimax_search(gametree_node *&root, 
+                    int ln,     // 表示层数，第一次调用时传入 0
+                    int &y, 
+                    int &x) {
 
+    if (ln == LAYER_NUM + 1) return;
+    
+    int our_color = setblack ? BLACK : WHITE;
+    int enemy_color = setblack ? WHITE : BLACK;    
+
+    /*
+     * 初始化节点
+     */
     root = (gametree_node *)malloc(sizeof(gametree_node));
-    root->x = root->y = -1;
-    root->_x = root->_y = -1;
-    root->_score = -1;
-    root->layer_number = layer_number;
-    root->prev = layer_number ? container_of(&root, gametree_node, next) : nullptr;
+    root->x = root->y = INT_MIN;
+    root->_x = root->_y = INT_MIN;
+    root->_score = INT_MIN;
+    root->layer_number = ln;
+    root->prev = ln ? container_of(&root, gametree_node, next) : nullptr;
     root->next = nullptr;
 
-    // 将上一级的 chessboard 复制到当前节点的 chessboard 中
-    int (*ptmp)[CK_SIZE] = layer_number ? root->prev->chessboard : position;
-    for (int i = 0; i < CK_SIZE; i++) {
-        copy(ptmp[i], ptmp[i] + CK_SIZE, root->chessboard[i]);
-    }
+    /*
+     * 将上一级的 chessboard 复制到当前节点的 chessboard 中，
+     * 如果当前节点为第 0 层，则复制 position 的内容
+     */
+    int (*p)[CK_SIZE] = ln ? root->prev->chessboard : position;
+    for (int i = 0; i < CK_SIZE; i++)
+        copy(p[i], p[i] + CK_SIZE, root->chessboard[i]);
 
-    // 记录每一层最终选出的落子点及分数
     int dummy_score = INT_MIN;
-    int dummy_x = -1, dummy_y = -1;
+    int dummy_x = INT_MIN, dummy_y = INT_MIN;
 
+    if (ln == 0) minimax_search(root->next, ln + 1, y, x);
+    /*
+     * 遍历每一个空位，产生所有可能的走法
+     */
     for (int i = 0; i < CK_SIZE; i++) {
         for (int j = 0; j < CK_SIZE; j++) {
-            if (root->chessboard[i][j] == 0) {
-                // 将有空位的点临时放置一个子
-                root->x = j;
+            if (root->chessboard[i][j] == EMPTY && ln != 0) {
                 root->y = i;
-                root->chessboard[i][j] = layer_number % 2 ? BLACK : WHITE;
+                root->x = j;
+                root->chessboard[i][j] = ln % 2 ? our_color : enemy_color;
 
-                minimax_search(root->next, layer_number + 1, y, x);
-                int tmp_score = evaluation(i, j, root->chessboard, BLACK);
+                minimax_search(root->next, ln + 1, y, x);
+                int eval_score;
 
-                if (LAYER_NUM == layer_number) {
-                    if (tmp_score > dummy_score || INT_MIN == dummy_score) {
-                        dummy_score = tmp_score;
-                        dummy_x = root->x;
+                /*
+                 * 如果搜索到最后一层，则对每该局面估值，
+                 * 否则选取下一层节点反馈给该层的分数
+                 */
+                if (ln == LAYER_NUM) {
+                    eval_score = evaluation(i, j, root->chessboard, 
+                                            ln % 2 ? our_color : enemy_color) - 
+                                 evaluation(root->prev->y, root->prev->x, 
+                                            root->chessboard, 
+                                            ln % 2 ? enemy_color : our_color);
+                    /*
+                     * 如果是奇数层，则选出最小值反馈给上层，
+                     * 否则选出最大值反馈给上层
+                     */
+                    if (ln % 2 && (dummy_score == INT_MIN || 
+                        eval_score < dummy_score)) {
+                        dummy_score = eval_score;
                         dummy_y = root->y;
-                    }
-                } else if (layer_number == 0) {
-                    if (root->_score > dummy_score || INT_MIN == dummy_score) {
-                        dummy_score = root->_score;
-                        x = root->x;
-                        y = root->y;
+                        dummy_x = root->x;
+                    } else if (ln % 2 == 0 && (dummy_score == INT_MIN || 
+                        eval_score > dummy_score)) {
+                        dummy_score = eval_score;
+                        dummy_y = root->y;
+                        dummy_x = root->x;
                     }
                 } else {
-                    if (layer_number % 2 == 0) {
-                        if (root->_score > dummy_score || INT_MIN == dummy_score) {
-                            dummy_score = root->_score;
-                            dummy_x = root->x;
-                            dummy_y = root->y;
-                        }
-                    } else {
-                        if (root->_score < dummy_score || INT_MIN == dummy_score) {
-                            dummy_score = root->_score;
-                            dummy_x = root->x;
-                            dummy_y = root->y;
-                        }
+                    if (ln % 2 && (dummy_score == INT_MIN || 
+                        root->_score < dummy_score)) {
+                        dummy_score = root->_score;
+                        dummy_y = root->y;
+                        dummy_x = root->x;
+                    } else if (ln % 2 == 0 && (dummy_score == INT_MIN || 
+                        root->_score > dummy_score)) {
+                        dummy_score = root->_score;
+                        dummy_y = root->y;
+                        dummy_x = root->x;
                     }
                 }
-                 
-                root->chessboard[i][j] = 0;
+                root->chessboard[i][j] = EMPTY;
             }
         }
     }
-
-    if (layer_number != 0) {
-        root->prev->_x = dummy_x;
-        root->prev->_y = dummy_y;
-        root->prev->_score = dummy_score;
-    } else {
-        x = dummy_x;
-        y = dummy_y;
-    }
     
+    if (ln != 0) {
+        root->prev->_score = dummy_score;
+        root->prev->_y = dummy_y;
+        root->prev->_x = dummy_x;
+    } else {
+        y = root->y;
+        x = root->x;
+    }
+
     free(root);
     root = nullptr;
-}
+}   
 
 /*
  * 评估函数，只针对某一方估值
  */
-int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color) {
+int evaluation(const int _y, const int _x, 
+               int (*chessboard)[CK_SIZE], int color) {
     char buf[8];
-    int max_score = 0, dummy_score = 0;
+    int max_score = 0, dummy_score = INT_MIN;
     
     /*
      * 先横向分析
@@ -123,14 +160,23 @@ int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color) {
             else buf[j] = ENEMY;
         }
 
-        for (int j = 0; j < MAX_SIZE - MIN_SIZE; j++) {
+        for (int j = 0; j <= MAX_SIZE - MIN_SIZE; j++) {
             if ((dummy_score = chess_type[buf]) != 0) 
                 break;
-            if (chessboard[_y][i + MIN_SIZE + j] == EMPTY) 
+            if (chessboard[_y][i + MIN_SIZE + j] == EMPTY && 
+                i + MIN_SIZE + j < CK_SIZE &&
+                j < MAX_SIZE - MIN_SIZE) 
                 buf[MIN_SIZE + j] = NO_PIECE;
-            else if (chessboard[_y][i + MIN_SIZE + j] == color) 
+
+            else if (chessboard[_y][i + MIN_SIZE + j] == color && 
+                    i + MIN_SIZE + j < CK_SIZE && 
+                    j < MAX_SIZE - MIN_SIZE) 
                 buf[MIN_SIZE + j] = OUR;
-            else
+
+            else if (chessboard[_y][i + MIN_SIZE + j] != color &&
+                    chessboard[_y][i + MIN_SIZE + j] != EMPTY && 
+                    i + MIN_SIZE + j < CK_SIZE &&
+                    j < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + j] = ENEMY;
         }
 
@@ -149,14 +195,23 @@ int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color) {
             else buf[j] = ENEMY;
         }
 
-        for (int j = 0; j < MAX_SIZE - MIN_SIZE; j++) {
+        for (int j = 0; j <= MAX_SIZE - MIN_SIZE; j++) {
             if ((dummy_score = chess_type[buf]) != 0) 
                 break;
-            if (chessboard[i + MIN_SIZE + j][_x] == 0) 
+            if (chessboard[i + MIN_SIZE + j][_x] == EMPTY &&
+                i + MIN_SIZE + j < CK_SIZE &&
+                j < MAX_SIZE - MIN_SIZE) 
                 buf[MIN_SIZE + j] = NO_PIECE;
-            else if (chessboard[i + MIN_SIZE + j][_x] == color)
+
+            else if (chessboard[i + MIN_SIZE + j][_x] == color &&
+                    i + MIN_SIZE + j < CK_SIZE &&
+                    j < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + j] = OUR;
-            else
+
+            else if (chessboard[i + MIN_SIZE + j][_x] != color &&
+                    chessboard[i + MIN_SIZE + j][_x] != EMPTY &&
+                    i + MIN_SIZE + j < CK_SIZE &&
+                    j < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + j] = ENEMY;
         }
         
@@ -181,11 +236,23 @@ int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color) {
         for (int k = 0; k < MAX_SIZE - MIN_SIZE; j++) {
             if ((dummy_score = chess_type[buf]) != 0)
                 break;
-            if (chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] == EMPTY)
+            if (chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] == EMPTY &&
+                i + MIN_SIZE + k < CK_SIZE && 
+                j + MIN_SIZE + k < CK_SIZE && 
+                k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + 1] = NO_PIECE;
-            else if (chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] == color)
+
+            else if (chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] == color &&
+                    i + MIN_SIZE + k < CK_SIZE &&
+                    j + MIN_SIZE + k < CK_SIZE &&
+                    k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + k] = OUR;
-            else
+
+            else if (chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] != color &&
+                    chessboard[i + MIN_SIZE + k][j + MIN_SIZE + k] != EMPTY &&
+                    i + MIN_SIZE + k < CK_SIZE && 
+                    j + MIN_SIZE + k < CK_SIZE &&
+                    k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + k] = ENEMY;
         }
 
@@ -209,11 +276,23 @@ int evaluation(int _y, int _x, int (*chessboard)[CK_SIZE], int color) {
         for (int k = 0; k < MAX_SIZE - MIN_SIZE; k++) {
             if ((dummy_score = chess_type[buf]) != 0)
                 break;
-            if (chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] == EMPTY)
+            if (chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] == EMPTY &&
+                i - MIN_SIZE - k >= 0 && 
+                j + MIN_SIZE + k < CK_SIZE &&
+                k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + k] = NO_PIECE;
-            else if (chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] == color)
+
+            else if (chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] == color &&
+                    i - MIN_SIZE - k >= 0 && 
+                    j + MIN_SIZE + k < CK_SIZE &&
+                    k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + k] = OUR;
-            else
+
+            else if (chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] != color && 
+                    chessboard[i - MIN_SIZE - k][j + MIN_SIZE + k] != EMPTY &&
+                    i - MIN_SIZE - k >= 0 && 
+                    j + MIN_SIZE + k < CK_SIZE &&
+                    k < MAX_SIZE - MIN_SIZE)
                 buf[MIN_SIZE + k] = ENEMY;
         }
 
